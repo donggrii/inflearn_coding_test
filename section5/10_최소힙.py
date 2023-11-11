96c9d21f60a284211b898c30548ad27cdce5bda6
donggrii <ld4230@gmail.com>
2023-11-11 18:22:09 +0900

    feat: Add section5 10th solution

---
+++ section5/10_최소힙.py
@@ -0,0 +1,55 @@
+# 내 풀이 (정답)
+import sys
+import heapq
+
+
+sys.stdin = open("input.txt", "r")
+heap = []
+while True:
+    a = int(input())
+    if a == -1:
+        break
+    elif a == 0:
+        with open("output10.txt", "a") as f:
+            print(-1 if len(heap) == 0 else heapq.heappop(heap), file=f)
+    else:
+        heapq.heappush(heap, a)
+
+
+# 정답 해설
+import sys
+import heapq as hq
+
+
+sys.stdin = open("input.txt", "r")
+a = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(hq.heappop(a))
+    else:
+        hq.heappush(a, n)
+
+
+# Test Case.
+# < input >
+# 5
+# 3
+# 6
+# 0
+# 5
+# 0
+# 2
+# 4
+# 0
+# -1
+
+# < output >
+# 3
+# 5
+# 2
