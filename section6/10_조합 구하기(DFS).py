e8c35984c0f2366d089ecf965600e67c4e8b1395
donggrii <ld4230@gmail.com>
2024-01-01 17:26:45 +0900

    feat: Add section6 10th solution

--- section6/10_조합 구하기(DFS).py
+++ section6/10_조합 구하기(DFS).py
@@ -1,54 +1,76 @@
-# 내 풀이 (실패.. -> 설명 듣고 다시 해보기 -> 성공!)
-def dfs(L, s):
-    """
-    :param L: Level
-    :param s: Start (s부터 가지가 뻗기 시작)
-    :return:
-    """
+# 내 풀이 (성공)
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(level, start):
     global cnt
-    if L == m:
-        for j in res:
-            print(j, end=" ")
+    if level == m:
+        for x in range(m):
+            print(result[x], end=" ")
         print()
         cnt += 1
     else:
-        for i in range(s, n + 1):
+        for i in range(start, n + 1):
             if check[i] == 0:
                 check[i] = 1
-                res[L] = i
-                dfs(L + 1, i + 1)
+                result[level] = i
+                DFS(level + 1, i + 1)
                 check[i] = 0
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    res = [0] * m
     check = [0] * (n + 1)
+    result = [0] * m
     cnt = 0
-    dfs(0, 1)
+    DFS(0, 1)
     print(cnt)
 
 
-# 답안 예시
-# "조합 구하기" 굉장히 중요함!! -> 이 문제를 기본으로 해서 많은 문제들이 응용으로 출제되므로 잘 기억해둘 것!!
-# 순열 구하는 것과 코드는 매우 유사하지만, 상태 트리의 유형이 다름!!
-# => start를 의미하는 s 변수가 추가되고, check 리스트는 필요 X
-def DFS(L, s):
+# 정답 해설
+# [조합 구하기] 문제 매우 중요! (기본이 되어 많이 응용되어 출제됨)
+# - [순열 구하기]와 코드는 유사하지만, 상태 트리의 유형이 다름 => start 변수가 추가되고, check 변수 사용 X
+# (참고) 조합에서는 왜 check 변수가 필요 없을까? (순열 vs. 조합)
+#        순열 : (1, 2), (1, 3), (1, 4), "(2, 1)"
+#        조합 : (1, 2), (1, 3), (1, 4), "(2, 3)"
+#        => 조합은 (2, 1)처럼 사용했던 숫자를 다시 쓰지 않고, start 변수에 따라 어차피 순서대로 조합을 구하기 때문
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(level, start):
     global cnt
-    if L == m:
-        for j in range(L):
-            print(res[j], end=" ")
+    if level == m:
+        for j in range(m):
+            print(result[j], end=" ")
         print()
         cnt += 1
     else:
-        for i in range(s, n + 1):
-            res[L] = i
-            DFS(L + 1, i + 1)
+        for i in range(start, n + 1):
+            result[level] = i
+            DFS(level + 1, i + 1)
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    res = [0] * m
+    result = [0] * m
     cnt = 0
     DFS(0, 1)
     print(cnt)
+
+
+# Test Case.
+# < input >
+# 4 2
+
+# < output >
+# 1 2
+# 1 3
+# 1 4
+# 2 3
+# 2 4
+# 3 4
+# 6
