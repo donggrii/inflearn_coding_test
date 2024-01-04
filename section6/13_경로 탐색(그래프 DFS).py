b473af69a55463fe321c46a5802a559239b1ae58
donggrii <ld4230@gmail.com>
2024-01-04 15:14:24 +0900

    feat: Add section6 13th solution

--- section6/13_경로 탐색(그래프 DFS).py
+++ section6/13_경로 탐색(그래프 DFS).py
@@ -1,4 +1,40 @@
-# 내 풀이 (성공!)
+# 내 풀이 (성공)
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(vertex):
+    global cnt
+    if vertex == n:
+        cnt += 1
+    else:
+        for i in range(1, n + 1):
+            if check[i] == 0 and graph[vertex][i] == 1:
+                check[i] = 1
+                DFS(i)
+                check[i] = 0
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    graph = [[0] * (n + 1) for _ in range(n + 1)]
+    for _ in range(m):
+        start, end = map(int, input().split())
+        graph[start][end] = 1
+    cnt = 0
+    check = [0] * (n + 1)
+    check[1] = 1
+    DFS(1)
+    print(cnt)
+
+
+# (과거) 내 풀이 (성공)
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
 def dfs(v):
     global cnt
     if v == n:
@@ -24,36 +60,40 @@ if __name__ == "__main__":
     print(cnt)
 
 
-# 답안 예시
-# 문제에는 주어지지 않았지만, 재방문은 허용 X (만약 허용한다면, 1 ~ 2번을 무한정 왔다갔다 할 수 있기 때문)
-# => check 리스트에 방문했다는 것을 표시해줘야 함!
-def DFS(v):  # v : 노드 번호
+# 정답 해설
+# 그래프 이론에서 "경로"의 정의 : 한 번 방문한 노드는 재방문 하지 않음
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(vertex):
     global cnt
-    if v == n:
+    if vertex == n:
         cnt += 1
-        for x in path:
-            print(x, end=" ")
-        print()
+        # for x in path:
+        #     print(x, end=" ")
+        # print()
     else:
-        for i in range(1, n + 1):  # i : 방문하려고 하는 노드 번호
-            if g[v][i] == 1 and ch[i] == 0:
-                ch[i] = 1
-                path.append(i)  # 경로 추가
+        for i in range(1, n + 1):
+            if graph[vertex][i] == 1 and check[i] == 0:
+                check[i] = 1
+                # path.append(i)
                 DFS(i)
-                path.pop()  # 되돌아갈 때 pop 반드시 해줘야 함!
-                ch[i] = 0
+                # path.pop()
+                check[i] = 0
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    g = [[0] * (n + 1) for _ in range(n + 1)]
-    ch = [0] * (n + 1)  # 방문 체크 리스트
-    for i in range(m):
-        a, b = map(int, input().split())
-        g[a][b] = 1
+    graph = [[0] * (n + 1) for _ in range(n + 1)]
+    check = [0] * (n + 1)  # 방문 체크 리스트
+    for _ in range(m):
+        start, end = map(int, input().split())
+        graph[start][end] = 1
     cnt = 0
-    path = [1]  # 실제로 원하는 경로대로 갔는지 "경로"까지 출력해보기
-    ch[1] = 1  # 출발 지점 방문 표시
+    # path = [1]
+    check[1] = 1  # 출발 지점 방문 표시
     DFS(1)
     print(cnt)
 
@@ -73,9 +113,3 @@ if __name__ == "__main__":
 
 # < output >
 # 6
-# [[0, 0, 0, 0, 0, 0],
-#  [0, 0, 1, 1, 1, 0],
-#  [0, 1, 0, 1, 0, 1],
-#  [0, 0, 0, 0, 1, 0],
-#  [0, 0, 1, 0, 0, 1],
-#  [0, 0, 0, 0, 0, 0]]
