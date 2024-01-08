4531acc6009b21abf802216a07eba64c8303019d
donggrii <ld4230@gmail.com>
2024-01-08 17:50:42 +0900

    feat: Add section7 1st solution

--- section7/1_최대점수 구하기(DFS).py
+++ section7/1_최대점수 구하기(DFS).py
@@ -1,83 +1,77 @@
-# # 내 풀이 1 (일부 성공, 일부 실패)
-# # Cut Edge 더 할 수 있는 방법을 모르겠음..
-# def dfs(L, sum_, time):
-#     global answer
-#     if answer < sum_:
-#         answer = sum_
-#     if L == n:
-#         return
-#     else:
-#         for i in range(n):
-#             if ch[i] == 0 and time - q[i][1] >= 0:
-#                 ch[i] = 1
-#                 dfs(L + 1, sum_ + q[i][0], time - q[i][1])
-#                 ch[i] = 0
-#
-#
-# if __name__ == "__main__":
-#     n, m = map(int, input().split())
-#     q = [list(map(int, input().split())) for _ in range(n)]
-#     answer = 0
-#     ch = [0] * n
-#     dfs(0, 0, m)
-#     print(answer)
+# 내 풀이 (수정) (실패 : 2 ~ 5번 TC 시간초과 or 오답)
+# 전체 집합 중 조건에 맞는 부분 집합 만들기 => DFS 적용
+# 회고 : 각 문제를 선택할 지 말 지 하나씩(level) 선택하면 되기 때문에, for문을 사용할 필요가 없음
+import sys
+
+sys.stdin = open("input.txt", "r")
 
 
-# 내 풀이 2 (5가닥이 아니라 2가닥씩 뻗으면 된다는 설명 듣고 다시 풀어보기 -> 성공!)
-# (참고) DFS 문제 같으면,
-# 1. 2가닥으로 해결할 수 있는지 보고,
-# 2. 안될 것 같으면 여러 가닥으로 뻗는 것 생각하기
-def dfs(L, sum_, time):
-    global answer
-    if time > m:
+def DFS(level, total_score, total_time, judged_score):
+    """
+    Args:
+        level (int): 문제 번호
+        total_score (int): 문제를 풀었다면 얻는 점수
+        total_time (int): 문제 푸는 데 할애한 시간
+        judged_score (int): 부분 집합에 포함할 지 판단 여부가 끝난 총 점수
+    """
+    global max_score
+    if total_time > m:
         return
-    if L == n:
-        if answer < sum_:
-            answer = sum_
+    if (sum_total_score - judged_score) + total_score < max_score:
+        return
+    if level == n:
+        max_score = max(max_score, total_score)
     else:
-        dfs(L + 1, sum_ + q[L][0], time + q[L][1])
-        dfs(L + 1, sum_, time)
+        # for score, time in probs:
+        #     DFS(level + 1, total_score + score, total_time + time, judged_score + score)
+        #     DFS(level + 1, total_score, total_time, judged_score + score)
+        DFS(
+            level + 1,
+            total_score + probs[level][0],
+            total_time + probs[level][1],
+            judged_score + probs[level][0],
+        )
+        DFS(level + 1, total_score, total_time, judged_score + probs[level][0])
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    q = [list(map(int, input().split())) for _ in range(n)]
-    answer = 0
-    dfs(0, 0, 0)
-    print(answer)
+    probs = sorted([tuple(map(int, input().split())) for _ in range(n)], reverse=True)
+    sum_total_score = sum(score for score, _ in probs)
+    max_score = 0
+    DFS(0, 0, 0, 0)
+    print(max_score)
 
 
-# 답안 예시
-# 각각 문제를 풀 건지, 안 풀 건지 결정하는 문제 => 부분집합 만들기! (2가닥씩 뻗으면 됨)
-def DFS(L, sum, time):
-    """
-    :param L: 문제 번호
-    :param sum: 문제를 풀었다면 얻는 점수
-    :param time: 문제 푸는 데 할애한 시간
-    :return:
-    """
-    global res
+# 정답 해설
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(level, sum_, time):
+    global result
     if time > m:  # 가지치기
         return
-    if L == n:  # 문제를 다 적용해서 부분집합 하나가 완성됐을 때
-        if sum > res:
-            res = sum
+    if level == n:  # 부분집합 하나가 완성됐을 때
+        if sum_ > result:
+            result = sum_
     else:
-        DFS(L + 1, sum + pv[L], time + pt[L])
-        DFS(L + 1, sum, time)
+        DFS(level + 1, sum_ + prob_value[level], time + prob_time[level])
+        DFS(level + 1, sum_, time)
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    pv = list()  # problem value
-    pt = list()  # problem time
-    for i in range(n):
-        a, b = map(int, input().split())
-        pv.append(a)
-        pt.append(b)
-    res = -2147000000
+    prob_value = []
+    prob_time = []
+    for _ in range(n):
+        score, time = map(int, input().split())
+        prob_value.append(score)
+        prob_time.append(time)
+    result = -2147000000
     DFS(0, 0, 0)
-    print(res)
+    print(result)
 
 
 # Test Case.
@@ -89,4 +83,5 @@ if __name__ == "__main__":
 # 6 3
 # 7 4
 
-# output : 41
+# < output >
+# 41
