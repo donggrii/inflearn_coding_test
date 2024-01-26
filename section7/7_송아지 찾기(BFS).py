66f4dd7cbaec43ebe35153774ddd8e1b909b2c31
donggrii <ld4230@gmail.com>
2024-01-26 13:00:33 +0900

    feat: Add section7 7th solution

--- section7/7_송아지 찾기(BFS).py
+++ section7/7_송아지 찾기(BFS).py
@@ -1,47 +1,124 @@
-# 내 풀이 (설명만 듣고 다시 풀기 -> 성공!)
-# 현재 위치 기준으로 푼 내 풀이보다 다음 위치 기준으로 푼 답안이 더 좋아보임
+# 내 풀이 (실패, 60점, 4 ~ 5번 TC 시간초과)
+# 한 번 방문한 곳은 체크해두고 다시 방문하지 않을 때 시간 안에 풀 수 있음
+import sys
 from collections import deque
 
+sys.stdin = open("input.txt", "r")
 
-s, e = map(int, input().split())
-ch = [0] * 10001
-dist = [0] * 10001
-q = deque([(s, 0)])
-while q:
-    cur, res = q.popleft()
-    if cur == e:
-        print(res)
+start, end = map(int, input().split())
+jumps = (1, -1, 5)
+
+queue = deque()
+queue.append((0, start))
+
+while queue:
+    count, position = queue.popleft()
+    if position == end:
+        print(count)
+        queue.clear()
         break
-    if 0 <= cur <= 10000 and ch[cur] == 0:  # 현재 위치 기준
-        ch[cur] = 1
-        dist[cur] = res
-        for i in [1, -1, 5]:
-            q.append((cur + i, res + 1))
+    count += 1
+    for i in jumps:
+        queue.append((count, position + i))
 
 
-# 답안 예시
+# 내 풀이 2 (성공)
+# 1. 강의 이론만 듣고 다시 풀어본 풀이, 처음에는 실패(80점, 5번 TC 답 안 나옴)
+# 2. jumped 구할 때 (current + i)의 범위를 0 ~ 10000으로 한정해줬을 때 성공
+import sys
 from collections import deque
 
+sys.stdin = open("input.txt", "r")
 
-n, m = map(int, input().split())
+start, end = map(int, input().split())
+jumps = (1, -1, 5)
+distance = [0] * 10001  # 직선의 좌표 점은 1부터 10,000까지 존재
+check = [0] * 10001
+check[start] = 1
+
+queue = deque([start])
+
+while queue:
+    current = queue.popleft()
+    if current == end:
+        print(distance[current])
+        break
+    for i in jumps:
+        jumped = (current + i) if 0 <= (current + i) <= 10000 else start
+        if check[jumped] == 0:
+            distance[jumped] = distance[current] + 1
+            check[jumped] = 1
+            queue.append(jumped)
+
+
+# 정답 해설
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt", "r")
+
 MAX = 10000
-ch = [0] * (MAX + 1)
-dis = [0] * (MAX + 1)
-ch[n] = 1  # 방문 표시
-dis[n] = 0
-dQ = deque([n])
-while dQ:
-    now = dQ.popleft()
-    if now == m:
+check = [0] * (MAX + 1)
+distance = [0] * (MAX + 1)
+start, end = map(int, input().split())
+check[start] = 1  # 방문 표시
+distance[start] = 0
+
+queue = deque()
+queue.append(start)
+
+while queue:
+    now = queue.popleft()
+    if now == end:
         break
-    for next in (now - 1, now + 1, now + 5):  # 다음 위치 기준
-        if 0 < next <= MAX and ch[next] == 0:
-            dQ.append(next)
-            ch[next] = 1
-            dis[next] = dis[now] + 1
-print(dis[m])
+    for next in (now - 1, now + 1, now + 5):
+        if 0 < next <= MAX:
+            if check[next] == 0:
+                queue.append(next)
+                check[next] = 1
+                distance[next] = distance[now] + 1
+
+print(distance[end])
+
+
+# 정답 해설 2
+# for문에서 도착 지점 발견 시 바로 종료하기
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt", "r")
+
+MAX = 10000
+check = [0] * (MAX + 1)
+distance = [0] * (MAX + 1)
+start, end = map(int, input().split())
+check[start] = 1
+distance[start] = 0
+
+queue = deque()
+queue.append(start)
+flag = False
+
+while queue:
+    now = queue.popleft()
+    for next in (now - 1, now + 1, now + 5):
+        if 0 < next <= MAX:
+            if check[next] == 0:
+                queue.append(next)
+                check[next] = 1
+                distance[next] = distance[now] + 1
+                if next == end:
+                    flag = True
+                    break
+    if flag:
+        break
+
+print(distance[end])
 
 
 # Test Case.
-# input : 5 14
-# output : 3
+# < input >
+# 5 14
+
+# < output >
+# 3
