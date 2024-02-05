c16b6cb279c48209bac4228c7f00c3ae60b36bbe
donggrii <ld4230@gmail.com>
2024-02-05 16:48:45 +0900

    feat: Add section7 8th solution

--- section7/8_사과나무(BFS).py
+++ section7/8_사과나무(BFS).py
@@ -1,75 +1,105 @@
-# # 내 풀이 (실패..)
-# from collections import deque
-#
-#
-# n = int(input())
-# arr = [list(map(int, input().split())) for _ in range(n)]
-# it = n // 2
-#
-# q = deque([(it, it)])
-# ch = [[0] * n for _ in range(n)]
-# ch[it][it] = 1
-# res = 0  # 총 사과 개수
-# dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-# idx = 0
-# while idx <= it:
-#     x, y = q.popleft()
-#     res += arr[x][y]
-#     for i in range(4):
-#         nx, ny = x + dx[i], y + dy[i]
-#         if 0 <= nx < n and 0 <= ny < n and ch[nx][ny] == 0:
-#             ch[nx][ny] = 1
-#             q.append((nx, ny))
-#     idx += 1
-#
-# while q:
-#     x, y = q.popleft()
-#     res += arr[x][y]
-#
-# print(res)
-
-
-# 답안 예시
-# BFS : Level 기준으로 탐색
-# 시작 지점 -> 방문 처리, Queue에 추가하는 등의 처리는 미리 해두고 시작하는 편
-# Queue, Check, Sum 3단계!
-# [n // 2, n // 2] : 격자의 중심점
+# 내 풀이 1 (정답)
+# BFS 풀이, 수확하지 않는 부분(제약조건) 표시
+import sys
 from collections import deque
 
+sys.stdin = open("input.txt", "r")
 
 n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
+apples = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = (-1, 1, 0, 0), (0, 0, -1, 1)  # 상하좌우
+half_point = n // 2
+check = [[0] * n for _ in range(n)]
+
+# 수확하지 않는 부분(제약조건) -1로 채우기
+for i in range(1, half_point + 1):
+    for j in range(i):
+        # 행 방향으로 반 나눠서 위 양쪽 부분 -1로 채우기
+        check[half_point - i][j] = -1  # (1, 0) / (0, 0), (0, 1)
+        check[half_point - i][-j - 1] = -1  # (1, -1) / (0, -1), (0, -2)
+        # 행 방향으로 반 나눠서 아래 양쪽 부분 -1로 채우기
+        check[half_point + i][j] = -1  # (3, 0) / (4, 0), (4, 1)
+        check[half_point + i][-j - 1] = -1  # (3, -1) / (4, -1), (4, -2)
+
+queue = deque()
+queue.append((half_point, half_point))  # (2, 2)
+check[half_point][half_point] = 1  # 처음 위치 방문 표시
+result = apples[half_point][half_point]  # 수확한 사과의 개수
+
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and check[nx][ny] == 0:
+            check[nx][ny] = 1
+            result += apples[nx][ny]
+            queue.append((nx, ny))
+
+print(result)
+
+
+# 내 풀이 2 (정답)
+# BFS 아닌 풀이, 특정 패턴이 있으므로 중간/위/아래 나눠서 바로 답 구하기
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+n = int(input())
+apples = [list(map(int, input().split())) for _ in range(n)]
+half_point = n // 2
+result = 0
+
+for i in range(n):
+    result += apples[half_point][i]  # 중간 : (2, 0) ~ (2, 4)
+
+for i in range(1, half_point + 1):
+    for j in range(n - (2 * i)):
+        result += apples[half_point - i][i + j]  # 위 : (1, 1), (1, 2), (1, 3) / (0, 2)
+        result += apples[half_point + i][i + j]  # 아래 : (3, 1), (3, 2), (3, 3) / (4, 2)
+
+print(result)
+
+
+# 정답 해설
+# - [Section 3] 7. 사과나무(다이아몬드)와 동일한 문제
+# - 가운데 지점 좌표(n // 2, n // 2)를 큐에 넣고, 상하좌우 4갈래로 상태트리를 뻗음 (level 기준 탐색)
+# - 종료지점은 level을 활용하여 설정
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt", "r")
+
+# 12시, 3시, 6시, 9시 (시계방향)
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-sum_ = 0
-ch = [[0] * n for _ in range(n)]
-Q = deque()
+n = int(input())  # 홀수 N
+apples = [list(map(int, input().split())) for _ in range(n)]
+check = [[0] * n for _ in range(n)]  # 방문 표시할 체크 리스트
 
-start = n // 2
-sum_ += a[start][start]
-ch[start][start] = 1
-Q.append((start, start))
-L = 0
+sum_apples = 0
+queue = deque()
+sum_apples += apples[n // 2][n // 2]
+check[n // 2][n // 2] = 1
+queue.append((n // 2, n // 2))
+level = 0
+
 while True:
-    if L == n // 2:
+    if level == n // 2:
         break
-    size = len(Q)
-    for i in range(size):
-        tmp = Q.popleft()
+    size = len(queue)
+    for _ in range(size):
+        tmp = queue.popleft()
         for j in range(4):
             x = tmp[0] + dx[j]
             y = tmp[1] + dy[j]
-            if ch[x][y] == 0:
-                sum_ += a[x][y]
-                ch[x][y] = 1
-                Q.append((x, y))
-    # print(L, size)
-    # for x in ch:
-    #     print(x)
-    L += 1
+            if check[x][y] == 0:
+                sum_apples += apples[x][y]
+                check[x][y] = 1
+                queue.append((x, y))
+    level += 1
 
-print(sum_)
+print(sum_apples)
 
 
 # Test Case.
@@ -81,4 +111,5 @@ print(sum_)
 # 19 27 29 37 27
 # 19 13 30 13 19
 
-# output : 379
+# < output >
+# 379
